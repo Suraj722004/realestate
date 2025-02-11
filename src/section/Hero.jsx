f5ccfe4120b2698820fea6a6263ab65a398bccb6
Suraj722004 <140174824+Suraj722004@users.noreply.github.com>
2025-02-11 11:08:37 +0530

    Update Hero.jsx

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { userDarkMode } from '../components/DarkModeContext'
-import heroimg from '../components/images/hero.jpg'
+import heroimg from '../components/images/herox.jpg'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
