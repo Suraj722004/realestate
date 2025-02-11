2bf101e74384323b1d6b201049aac295716183a7
Suraj722004 <140174824+Suraj722004@users.noreply.github.com>
2025-02-11 11:10:19 +0530

    Update About.jsx

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { userDarkMode } from '../components/DarkModeContext'
-import aboutimg from '../components/images/about.jfif'
+import aboutimg from '../components/images/hero.jpg'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
